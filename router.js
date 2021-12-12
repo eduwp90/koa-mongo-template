18ccc092fef277a4fcef2b9b7f6e011e94e663a0
eduwp90@gmail.com <eduwp90@gmail.com>
2021-12-12 22:53:34 +0100

    koa works

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
 const Router = require('koa-router');
 const router = new Router();
 
+const controller = require('./controllers/controller');
+
 router.get('/', controller.doSomething1);
 router.post('/', controller.doSomething2);
 
 //handle 404
-router.all('*', function (req,res) {
+router.all('', function (req,res) {
   res.status(404).send('not found');
 })
 
