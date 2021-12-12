b90ffe85ce61cc3bfe6cc232e7993925af6fb544
eduwp90 <eduwp90@gmail.com>
2021-12-12 23:39:49 +0100

    solved bugs

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,22 +3,22 @@ const model = require('../models/model');
 exports.doSomething1 = async (ctx) => {
   try {
     const data = await model.getData();
-    ctx.status(200);
-    ctx.body(data);
+    ctx.status = 200 ;
+    ctx.body = data;
   } catch (e) {
     console.log('e', e);
-    res.status(500);
+    ctx.status = 500;
   }
 };
 
 exports.doSomething2 = (ctx) => {
   try {
-    console.log(ctx.body)
-    model.postData(ctx.body.data);
-    ctx.status(201);
+    console.log(ctx.request.body)
+    model.postData(ctx.request.body.data);
+    ctx.status = 201;
 
   } catch (e) {
     console.log('e', e);
-    ctx.status(500);
+    ctx.status = 500;
   }
 };
--- router.js
+++ router.js
@@ -7,8 +7,9 @@ router.get('/', controller.doSomething1);
 router.post('/', controller.doSomething2);
 
 //handle 404
-router.all('', function (req,res) {
-  res.status(404).send('not found');
+router.all('', function (ctx) {
+  ctx.status(404);
+  ctx.body('not found');
 })
 
 module.exports = router;
